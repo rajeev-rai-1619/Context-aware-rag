95234b4777872c6bddd723b67fb4187582762497
rajeev-rai-1619 <rajeevrai6966@gmail.com>
2026-05-12 15:28:56 +0530

    Updated main.py and added mock for VextexAI

--- app/rag_pipeline.py
+++ app/rag_pipeline.py
@@ -2,15 +2,19 @@ from app.embedding_service import EmbeddingService
 from app.vector_store import VectorStore
 from app.retriever import Retriever
 from app.dataset import DatasetLoader
+from app.mock_models import MockGenerativeModel
 
 
 class RAGPipeline:
 
     def __init__(self):
+
         self.embedding_service = EmbeddingService()
 
         self.vector_store = None
         self.retriever = None
+
+        self.query_expander = MockGenerativeModel()
 
     def ingest_documents(self, file_path: str):
 
@@ -32,9 +36,34 @@ class RAGPipeline:
             vector_store=self.vector_store
         )
 
-    def search(self, query: str, top_k: int = 3):
+    # Strategy A: Direct Retrieval
+    def search_raw(
+        self,
+        query: str,
+        top_k: int = 3
+    ):
 
         return self.retriever.retrieve(
             query=query,
             top_k=top_k
         )
+
+    # Strategy B: Retrieval with Query Expansion
+    def search_expanded(
+        self,
+        query: str,
+        top_k: int = 3
+    ):
+
+        expanded_query = self.query_expander.generate_content(query)
+
+        results = self.retriever.retrieve(
+            query=expanded_query,
+            top_k=top_k
+        )
+
+        return {
+            "original_query": query,
+            "expanded_query": expanded_query,
+            "results": results
+        }
